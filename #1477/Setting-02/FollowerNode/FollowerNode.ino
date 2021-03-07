92820de4d97f7856c4fcfee6d47169b49108352c
Virtual Maker <56232456+virtual-maker@users.noreply.github.com>
2021-03-07 15:46:37 +0100

    Improve logic to send successful after
    4 failed messages to avoid transport layer reset

--- #1477/Setting-02/FollowerNode/FollowerNode.ino
+++ #1477/Setting-02/FollowerNode/FollowerNode.ino
@@ -16,7 +16,8 @@
 
 const uint32_t sendDelay = 0;
 
-#define SUCCESS_MESSAGE 5 //!< Each nth message shall be successful to avoid transport layer reset
+// After 4 failed messages the next one shall be sent successfully to avoid reset of transport layer
+#define MAX_FAILED_MESSAGES 4 
 
 #define SYNC_PIN 8 // Wire pin to connect with the leader node to synchronise both nodes
 
@@ -30,8 +31,8 @@ MyMessage msg(MY_CHILD_SENSOR_ID, V_STATUS);
 
 void presentation()
 {
-    sendSketchInfo("MySensors Follower Node", __DATE__);
-    present(MY_CHILD_SENSOR_ID, S_BINARY, "Receiver");
+	sendSketchInfo("MySensors Follower Node", __DATE__);
+	present(MY_CHILD_SENSOR_ID, S_BINARY, "Receiver");
 }
 
 void receive(const MyMessage& message)
@@ -40,64 +41,66 @@ void receive(const MyMessage& message)
 
 bool sendMessage()
 {
-    static const uint8_t payloadSize = MAX_PAYLOAD_SIZE;
-    static uint8_t data[payloadSize] = { 0 };
+	static const uint8_t payloadSize = MAX_PAYLOAD_SIZE;
+	static uint8_t data[payloadSize] = { 0 };
 
-    data[payloadSize - 1] = 42;
-    msg.set(data, payloadSize);
+	data[payloadSize - 1] = 42;
+	msg.set(data, payloadSize);
 
-    delay(sendDelay);
+	delay(sendDelay);
 
-    // Pin debug: send message
-    digitalWrite(DEBUG_PIN2, HIGH);
-    bool success = send(msg);
-    digitalWrite(DEBUG_PIN2, LOW);
-    
-    return success;
+	// Pin debug: send message
+	digitalWrite(DEBUG_PIN2, HIGH);
+	bool success = send(msg);
+	digitalWrite(DEBUG_PIN2, LOW);
+
+	return success;
 }
 
 void setup()
 {
-    // Activate synchronisation pin
-    pinMode(SYNC_PIN, INPUT);
+	// Activate synchronisation pin
+	pinMode(SYNC_PIN, INPUT);
 
-    // Activate debug pins
-    pinMode(DEBUG_PIN1, OUTPUT);
-    pinMode(DEBUG_PIN2, OUTPUT);
-    pinMode(DEBUG_PIN3, OUTPUT);
+	// Activate debug pins
+	pinMode(DEBUG_PIN1, OUTPUT);
+	pinMode(DEBUG_PIN2, OUTPUT);
+	pinMode(DEBUG_PIN3, OUTPUT);
 }
 
 void loop()
 {
-    static bool lastSync = LOW;
-    static uint8_t messageCounter = 0;
+	static bool lastSync = LOW;
+	static uint8_t messageCounter = 0;
 
-    // Pin debug: loop start
-    digitalWrite(DEBUG_PIN1, HIGH);
+	// Pin debug: loop start
+	digitalWrite(DEBUG_PIN1, HIGH);
 
-    bool thisSync = digitalRead(SYNC_PIN);
-    if (thisSync && !lastSync) {
-        // timeout counter to detect HW issues
-        uint16_t timeout = 0xFFFF;
-        // wait for falling edge
-        while (digitalRead(SYNC_PIN) && timeout--) {
-        }
-        // Let be sendMessage() successful each SUCCESS_MESSAGE
-        if (++messageCounter >= SUCCESS_MESSAGE) {
-            messageCounter = 0;
-            // Delay sendMessage to avoid collision
-            delay(100);
-        }
-        bool success = sendMessage();
-        if (!success) {
-            // Pin debug: send failed
-            digitalWrite(DEBUG_PIN3, HIGH);
-            delay(3);
-            digitalWrite(DEBUG_PIN3, LOW);
-        }
-    }
-    lastSync = thisSync;
+	bool thisSync = digitalRead(SYNC_PIN);
+	if (thisSync && !lastSync) {
+		// timeout counter to detect HW issues
+		uint16_t timeout = 0xFFFF;
+		// wait for falling edge
+		while (digitalRead(SYNC_PIN) && timeout--) {
+		}
+		// Let be sendMessage() successful after MAX_FAILED_MESSAGES
+		if (messageCounter >= MAX_FAILED_MESSAGES) {
+			// Delay sendMessage to avoid collision
+			delay(100);
+		}
+		bool success = sendMessage();
+		if (success) {
+			messageCounter = 0;
+		} else {
+			messageCounter++;
+			// Pin debug: send failed
+			digitalWrite(DEBUG_PIN3, HIGH);
+			delay(3);
+			digitalWrite(DEBUG_PIN3, LOW);
+		}
+	}
+	lastSync = thisSync;
 
-    // Pin debug: loop end
-    digitalWrite(DEBUG_PIN1, LOW);
+	// Pin debug: loop end
+	digitalWrite(DEBUG_PIN1, LOW);
 }
